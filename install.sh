cd67d052c55e5e5eff454950dc84d494f9d5121c
Tarek Mahfoudh <75225772+Tarekinh0@users.noreply.github.com>
2024-04-26 01:15:56 +0200

    Update install.sh
    Also need xdg-utils

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo apt update
 sudo apt install python-is-python3
 sudo apt install python3 
 sudo apt install python3-pip
-sudo apt install firefox
+sudo apt install firefox xdg-utils
 
 pip install -r requirements.txt
 mkdir generatedVideos
