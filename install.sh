b89a9a79b233914d74f97388af38bdc1b039c9d4
Tarek Mahfoudh <75225772+Tarekinh0@users.noreply.github.com>
2024-04-26 01:48:43 +0200

    Update install.sh
    Added imageMagick and the REAL firefox

--- install.sh
+++ install.sh
@@ -4,7 +4,20 @@ sudo apt update
 sudo apt install python-is-python3
 sudo apt install python3 
 sudo apt install python3-pip
-sudo apt install firefox xdg-utils
+sudo apt install xdg-utils
+sudo apt install imagemagick
+
+
+# installing firefox (not from apt or snap because marionette problems otherwise)
+sudo install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla
+sudo apt update && sudo apt install firefox
 
 pip install -r requirements.txt
 mkdir generatedVideos
