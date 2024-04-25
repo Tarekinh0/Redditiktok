c450ef7ec66d369eb33a2dc6da903027d07a23eb
Tarekinh0 <tarek@mahfoudh.net>
2024-04-25 17:29:26 +0200

    Fuck Google Translate's API

--- utils/getText.py
+++ utils/getText.py
@@ -1,15 +1,33 @@
 import httpcore
 setattr(httpcore, 'SyncHTTPTransport', 'AsyncHTTPProxy')
-
-from googletrans import Translator
 import xml.etree.ElementTree as ET
+import boto3
+import json
 
+BUCKET_NAME = "tiktelegram-bucket"
+DESTINATION_LANGUAGE = 'fr'
 
-
+with open('config.json', 'r') as config_file:
+    config = json.load(config_file)
 
 def translate_text(title, text):
-    translator = Translator(service_urls=['translate.googleapis.com'])
-    return translator.translate(title, src="en", dest='fr').text, translator.translate(text, dest='fr').text
+    
+    comprehend = boto3.client('comprehend', region_name="eu-west-1", aws_access_key_id = config['aws']['key'], aws_secret_access_key = config['aws']['token'])
+    response = comprehend.batch_detect_dominant_language(TextList=[title])
+    source_language = response["ResultList"][0]["Languages"][0]["LanguageCode"]
+
+    if source_language != DESTINATION_LANGUAGE:
+        translate = boto3.client('translate', region_name="eu-west-1", aws_access_key_id = config['aws']['key'], aws_secret_access_key = config['aws']['token'])
+        title = translate.translate_text(Text=title, SourceLanguageCode=source_language, TargetLanguageCode=DESTINATION_LANGUAGE )["TranslatedText"]
+        text = translate.translate_text(Text=text, SourceLanguageCode=source_language, TargetLanguageCode=DESTINATION_LANGUAGE )["TranslatedText"]
+    
+    return title, text
+
+#  deprecated
+# from googletrans import Translator
+# def translate_text(title, text):
+#     translator = Translator(service_urls=['translate.googleapis.com'])
+#     return translator.translate(title, src="en", dest='fr').text, translator.translate(text, dest='fr').text
 
 def split_into_chunks(title, text, chunk_length=1000):
     """Splits text into chunks where each is roughly <= chunk_length characters, 
