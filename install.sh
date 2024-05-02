584a596161ff7c7f9945cb4e0bcb9f487436ee6c
Tarek Mahfoudh <75225772+Tarekinh0@users.noreply.github.com>
2024-05-02 11:14:32 +0200

    Update install.sh
    Removed the sudos

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 
-sudo add-apt-repository main
-sudo apt update
-sudo apt install python-is-python3
-sudo apt install python3 
-sudo apt install python3-pip
-sudo apt install xdg-utils
-sudo apt install imagemagick libmagick++-dev
+# sudo add-apt-repository main
+apt update
+apt install python-is-python3
+apt install python3 
+apt install python3-pip
+apt install xdg-utils
+apt install imagemagick libmagick++-dev
 echo "[global]" >> ~/.config/pip/pip.conf
 echo "break-system-packages = true" >> ~/.config/pip/pip.conf
 
 
-sudo pip install --upgrade google-api-python-client --break-system-packages
-sudo pip install --upgrade google-auth-oauthlib google-auth-httplib2 --break-system-packages
+pip install --upgrade google-api-python-client --break-system-packages
+pip install --upgrade google-auth-oauthlib google-auth-httplib2 --break-system-packages
 
 
 sudo sed -i 's/rights="none"/rights="read | write"/' /etc/ImageMagick-6/policy.xml
@@ -28,20 +28,20 @@ Pin-Priority: 1000
 ' | sudo tee /etc/apt/preferences.d/mozilla
 sudo apt update && sudo apt install firefox
 
-sudo pip install -r requirements.txt --break-system-packages
+pip install -r requirements.txt --break-system-packages
 mkdir generatedVideos
 mkdir temp
 mkdir templateGamePlayVideos
 touch index.txt
 
-sudo pip install pytube --break-system-packages
+pip install pytube --break-system-packages
 
 cd templateGamePlayVideos
 sudo python ../other_scripts/youtubeDownload.py
 cd ..
 
-sudo pip install boto3 oauth2client lxml moviepy httpcore praw pysrt h11 webdriver_manager selenium bs4 aiohttp --break-system-packages
+pip install boto3 oauth2client lxml moviepy httpcore praw pysrt h11 webdriver_manager selenium bs4 aiohttp --break-system-packages
 
-echo "PLEASE DO NOT FORGET TO FILL IN default_config.json AND RENAMING IT TO config.json "
+echo "\n\nPLEASE DO NOT FORGET TO FILL IN default_config.json AND RENAMING IT TO config.json\n\n"
 
-echo "ADD IT TO BOOT IF YOU RUN IT ON A SERVER :)\n Then launch it manually right now with './bot.py &' or restart your machine"
+echo "ADD IT TO BOOT IF YOU RUN IT ON A SERVER :)\n Then launch it manually right now with './bot.py &' or restart your machine\n\n"
